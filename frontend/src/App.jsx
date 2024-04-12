2007010bab7f4eed9408eeeb2de5405a07d28424
Khushi Malhotra <malhotrakhushi507@gmail.com>
2024-04-12 14:35:23 +0530

    client side starting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import bg from "./img/bg.png";
+
+function App() {
+  return (
+    <>
+      <AppStyled bg={bg} className="App"></AppStyled>
+    </>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+`;
+
+export default App;
